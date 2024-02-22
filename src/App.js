d239e791bd66b27779acf983b1bc9f6c2921f7ac
John Crain <jcrain@Johns-MacBook-Pro.local>
2024-02-22 11:32:37 -0400

    Update favicon

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         On-chain Consulting LLC 
+        </p>
+        <p>We're a team of builders who specialize in web3 product development and work with companies like
+          <ul>
+            <li>SuperRare.com</li>
+            <li>Consensys</li>
+            <li>A couple top secret...</li>
+          </ul>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="mailto:hello@on-chainconsulting.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get in touch 👋 
         </a>
       </header>
     </div>
